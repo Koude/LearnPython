a38559648ffd65d18253ce9401239cefe859540d
Koude <Koude@users.noreply.github.com>
2018-02-09 19:47:24 +0800

    20180209

---
+++ Introduction to Computer Science and Programming Using Python/guessnumber.py
@@ -0,0 +1,18 @@
+low=0
+high=100
+
+guess = (low+high)//2
+judge = input("Is your secret number "+str(guess)+" ?"+"\nEnter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+
+while judge!="c":
+	
+	if judge == "l":
+		low = guess
+	elif judge == "h":
+	    high = guess
+	else:
+		print("Sorry, I did not understand your input.")
+	guess = (low+high)//2
+	judge = input("Is your secret number "+str(guess)+" ?"+"\nEnter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+
+print("Game over. Your secret number was: "+str(guess))
